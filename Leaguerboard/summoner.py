c0e150ca5b2b03df5d4fe07f96d4c1eb63be3816
deweesa <asadeweese@gmail.com>
2021-03-10 15:26:12 -0800

    Summoners page now acts as directory, no POST method
    Summoners no longer has the radio button post form, now
    has links for each summoner to a stat pages. Now that stat page
    might have a post to apply filters to the data but I think that
    makes more sense.

--- Leaguerboard/summoner.py
+++ Leaguerboard/summoner.py
@@ -3,7 +3,7 @@ from Leaguerboard.db import get_db
 
 bp = Blueprint('summoner', __name__)
 
-@bp.route('/summoner', methods=('GET', 'POST'))
+@bp.route('/summoners', methods=('GET', 'POST'))
 def summoner():
     if request.method == 'POST':
         return "Stat page is work in progress"
@@ -14,3 +14,17 @@ def summoner():
     ).fetchall()
     
     return render_template('summoner/summoner.html', summoners=summoners)
+
+@bp.route('/summoner/<string:summoner>')
+def summoner_stats(summoner):
+    db = get_db()    
+
+    game_count = db.execute(
+        'select count(1) from match where summonerName = ?', (summoner,)
+    ).fetchone()
+
+    win_count = db.execute(
+        'select count(1) from match where summonerName = ? and win = 1', (summoner,)
+    ).fetchone()
+
+    return 'games: ' + str(game_count[0]) + '\nwins: ' + str(win_count[0]) + "\npercentage: " + "%.2f%%" % (100 * win_count[0]*1.0/game_count[0])
