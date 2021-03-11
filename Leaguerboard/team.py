72046b49560c1aa3ab7e4a08ee77619dd60f4905
deweesa <asadeweese@gmail.com>
2021-03-11 15:14:29 -0800

    Team page now has functional form for selecting multiple summoners

---
+++ Leaguerboard/team.py
@@ -0,0 +1,18 @@
+from flask import (Blueprint, render_template, request)
+from Leaguerboard.db import get_db
+
+bp = Blueprint('team', __name__)
+
+@bp.route('/team', methods=('GET', 'POST'))
+def team():
+    db = get_db()
+
+    if request.method == 'POST':
+        if(len(request.form.getlist('summoner')) > 5):
+            return "Invalid team: More than 5 players (assuming a lot about potential games played)"
+
+        return str(request.form.getlist('summoner'))
+
+    summoners = db.execute('select summonerName from summoner').fetchall()
+
+    return render_template('team/team.html', summoners=summoners)
