5169440f22d42440acee812332fa95a156ea0463
Asa DeWeese <asadeweese@gmail.com>
2021-03-24 13:51:22 -0700

    getting some basic information for champions up, next steps are getting more complicated information like runes winrates per player

--- Leaguerboard/champion.py
+++ Leaguerboard/champion.py
@@ -12,5 +12,34 @@ def champions():
     with open('Leaguerboard/static/json/champion_full.json') as f:
         champ_dict = json.load(f)['data']
 
-#    return json.dumps(champ_dict['Aatrox'], indent=4)
+        champ_list = champ_dict.values()
     
+    return render_template('champion/champions.html', champions=champ_list)
+
+@bp.route('/champion/<string:champ>')
+def champion(champ):
+    with open('Leaguerboard/static/json/champion_full.json') as f:
+        champ_full = json.load(f)['data']
+
+    champ_dict = champ_full[champ]
+    key = champ_dict['key']
+
+    db = get_db()
+
+    game_count = db.execute('select count(*) from match where champion = ?', (key,)).fetchone()[0] 
+    win_count = db.execute('select count(*) from match where champion = ? and win = 1', (key,)).fetchone()[0]
+
+    return render_template('champion/champion.html', champ = champ_dict, game_count = game_count, win_count = win_count)
+
+
+def champ_winrate(champ_dict):
+    db = get_db()
+    key = champ_dict['key']
+    game_count = db.execute('select count(*) from match where champion = ?', (key,)).fetchone()[0]
+
+    if game_count == 0:
+        return 0
+
+    win_count = db.execute('select count(*) from match where champion = ? and win = 1', (key,)).fetchone()[0]
+
+    return 100.0 * win_count / game_count
