43f629c6dead3e48fac10c1a2f59ceeec522d891
Asa DeWeese <asadeweese@gmail.com>
2021-03-24 17:01:26 -0700

    Each champ now has a basic stat page that shows everyone that has played them, and what their winrates are

--- Leaguerboard/champion.py
+++ Leaguerboard/champion.py
@@ -11,6 +11,7 @@ def champions():
 
     with open('Leaguerboard/static/json/champion_full.json') as f:
         champ_dict = json.load(f)['data']
+        f.close()
 
         champ_list = champ_dict.values()
     
@@ -29,7 +30,19 @@ def champion(champ):
     game_count = db.execute('select count(*) from match where champion = ?', (key,)).fetchone()[0] 
     win_count = db.execute('select count(*) from match where champion = ? and win = 1', (key,)).fetchone()[0]
 
-    return render_template('champion/champion.html', champ = champ_dict, game_count = game_count, win_count = win_count)
+    players = db.execute('select distinct(summonerName) from match where champion = ?', (key,)).fetchall()
+
+    player_stats = []
+
+    for player in players:
+        stat_line = {}
+        stat_line['summonerName'] = player['summonerName']
+        stat_line['game_count'] = db.execute('select count(*) from match where champion = ? and summonerName = ?', (key, player['summonerName'])).fetchone()[0]
+        stat_line['win_count'] = db.execute('select count(*) from match where champion = ? and summonerName = ? and win = 1', (key, player['summonerName'])).fetchone()[0]
+        player_stats.append(stat_line)
+        
+    player_stats.sort(reverse=True, key=lambda stat_line: stat_line['game_count'])
+    return render_template('champion/champion.html', champ = champ_dict, game_count = game_count, win_count = win_count, player_stats = player_stats)
 
 
 def champ_winrate(champ_dict):
