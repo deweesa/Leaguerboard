9abffcc0c14a076ee79a1020046dc32ccbbe19d7
Asa DeWeese <asadeweese@gmail.com>
2021-03-29 17:03:07 -0700

    Player stats page has now been updated to use SQLAlchemy
    Today has a been just ORM growing pains. First had weird errors
    because I named the new sqlalchemy database instance 'db', which
    was running into name conflicts with 'db.py'. So got the SQLA instance
    name switched to 'database'. Was trying to declare models but was
    running into metadat issues with that. But I now have the SQLA instance
    running raw SQL for now. Tomorrow's todo is to get at least one of the
    other pages finished now that I know what SQLA wants of me. Want to have
    this up on herkoku by midday wednesday so I can share it with the boys
    and get their feedback on what features they'd like to see.

--- Leaguerboard/summoner.py
+++ Leaguerboard/summoner.py
@@ -1,30 +1,23 @@
 from flask import (Blueprint, render_template, request)
-from Leaguerboard.db import get_db
+#from Leaguerboard.db import get_db
+#from Leaguerboard.models import (Summoner, Match)
+from sqlalchemy import select, text
+from . import database
 
 bp = Blueprint('summoner', __name__)
 
 @bp.route('/summoners', methods=('GET', 'POST'))
 def summoner():
-    if request.method == 'POST':
-        return "Stat page is work in progress"
-    
-    db = get_db()
-    summoners = db.execute(
-        'select summonerName from summoner'
-    ).fetchall()
-    
+    with database.engine.connect() as conn:
+        summoners = conn.execute('select summonername from summoner').fetchall()
+
     return render_template('summoner/summoner.html', summoners=summoners)
 
 @bp.route('/summoner/<string:summoner>')
 def summoner_stats(summoner):
-    db = get_db()    
 
-    game_count = db.execute(
-        'select count(1) from match where summonerName = ?', (summoner,)
-    ).fetchone()[0]
-
-    win_count = db.execute(
-        'select count(1) from match where summonerName = ? and win = 1', (summoner,)
-    ).fetchone()[0]
+    with database.engine.connect() as conn:
+        game_count = conn.execute(text('select count(1) from match where summonername = :x'), x=summoner).fetchone()[0]
+        win_count = conn.execute(text('select count(1) from match where summonername = :x and win = true'), x = summoner).fetchone()[0]
 
     return render_template('summoner/summoner_stat.html', summoner=summoner, win_count=win_count, game_count=game_count)
