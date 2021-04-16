69212b777d1ab55ed45d44aa82a50775a4682be2
Asa DeWeese <asadeweese@gmail.com>
2021-04-16 16:03:42 -0700

    Now have the team page up to same functionality as on the main branch

--- Leaguerboard/team.py
+++ Leaguerboard/team.py
@@ -1,5 +1,4 @@
 from flask import (Blueprint, render_template, request, flash, redirect, url_for)
-#from Leaguerboard.db import get_db
 from sqlalchemy import (select, join, and_)
 from sqlalchemy.orm import aliased
 from . import database
@@ -9,8 +8,10 @@ bp = Blueprint('team', __name__)
 
 @bp.route('/team', methods=('GET', 'POST'))
 def team():
+    # TODO: - [ ] Rewrite the template for the page so that there aren't so 
+    #             many parameters
+    #       - [ ] Document
     if request.method == 'POST':
-        #get the account_id's of the selected team
         team_selection = request.form.getlist('summoner')
         error = None
 
@@ -22,8 +23,14 @@ def team():
             return "Invalid team: Please check more than one player"
         
         filtered_games = get_filtered_games(team_selection)
-        return str(team_statistics)
-        #return render_template('team/team_stats.html', game_count=game_count, win_count=win_count, team_selection=team_selection)
+        team_stats = get_stats(filtered_games)
+        team_names = get_names(team_selection)        
+
+
+        return render_template('team/team_stats.html', 
+                                game_count=team_stats['total'], 
+                                win_count=team_stats['win'], 
+                                team_names=team_names)
 
     # Get all the primary summoners and display them for team selection
     else:
@@ -35,6 +42,7 @@ def get_filtered_games(team_selection):
     # TODO: - [x] Get the games the teammates have played together
     #             (not exclusive)
     #       - [x] Take the games out that other primary summoners are in
+    #       - [ ] Write documentation 
     #       
     # Notes: - Thought about going back to the schmea and including the team
     #          team_id for each summoner, but realized that as long as they
@@ -61,10 +69,44 @@ def get_filtered_games(team_selection):
 
         participants = [p[0] for p in participants]
         participants.sort()
-        #print("Participants {}".format(participants))
 
         if participants == team_selection:
             filtered_games.append(game_id)
             continue
 
     return filtered_games
+
+def get_stats(games):
+    team_stats = {}
+    won_games = {}
+
+    s = select(MatchStat.game_id, MatchStat.account_id, MatchStat.win) \
+            .where(MatchStat.game_id.in_(games))
+
+    with database.engine.begin() as conn:
+        game_stats = conn.execute(s).fetchall()
+
+    for game_stat in game_stats:
+        if game_stat.game_id in won_games.keys():
+            if won_games[game_stat.game_id] != game_stat.win:
+                del won_games[game_stat.game_id]
+        else:
+            won_games[game_stat.game_id] = game_stat.win
+    
+    team_stats['total'] = len(won_games.values())
+    team_stats['win'] = 0
+    for result in won_games.values():
+        if result: team_stats['win'] += 1
+
+    return(team_stats)
+
+def get_names(selection):
+    # TODO: - [ ] Documentation
+    s = select(Summoner.name).where(Summoner.account_id.in_(selection))
+
+    with database.engine.begin() as conn:
+        names = conn.execute(s).fetchall()
+
+    names = [x[0] for x in names]
+
+    return names
