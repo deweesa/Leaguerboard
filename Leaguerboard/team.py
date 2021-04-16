5304cbeff344bf857a9f859a718714c53b2a5915
Asa DeWeese <asadeweese@gmail.com>
2021-04-16 14:28:14 -0700

    We are now getting games that only the specific team has participated in

--- Leaguerboard/team.py
+++ Leaguerboard/team.py
@@ -21,7 +21,7 @@ def team():
         if(len(team_selection) < 2):
             return "Invalid team: Please check more than one player"
         
-        team_statistics = get_team_stats(team_selection)
+        filtered_games = get_filtered_games(team_selection)
         return str(team_statistics)
         #return render_template('team/team_stats.html', game_count=game_count, win_count=win_count, team_selection=team_selection)
 
@@ -31,10 +31,10 @@ def team():
         return render_template('team/team.html', summoners=summoners)
 
 
-def get_team_stats(team_selection):
+def get_filtered_games(team_selection):
     # TODO: - [x] Get the games the teammates have played together
     #             (not exclusive)
-    #       - [ ] Take the games out that other primary summoners are in
+    #       - [x] Take the games out that other primary summoners are in
     #       
     # Notes: - Thought about going back to the schmea and including the team
     #          team_id for each summoner, but realized that as long as they
@@ -43,60 +43,28 @@ def get_team_stats(team_selection):
     #        - Might want a case for when the team_selection is 5. No need
     #          to go through and culling the leftovers.
     
-    #games = MatchStat.query.filter(MatchStat.account_id.in_(team_selection)).distinct().all()
-
-    # Select all the game_ids where players in team_selection participated. 
-    # Other primary summoner's could be part of these games at this point.
-    #s = select(MatchStat.game_id).\
-    #        where(MatchStat.account_id.in_(team_selection)).distinct()
-
-    #games = MatchStat.query.filter_by(account_id=team_selection[0])
-
+    team_selection.sort()
     s = select(MatchStat.game_id).where(MatchStat.account_id==team_selection[0])
-
+    
     with database.engine.begin() as conn:
        games = conn.execute(s)
 
     all_games = [x[0] for x in games]
-    game_count = 0
-
+    filtered_games = []
+    
     for game_id in all_games:
-        s = select(MatchStat.game_id, MatchStat.account_id) \
-                .where(
-                     and_(MatchStat.account_id!=team_selection[0],
-                          MatchStat.game_id==game_id,
-                          MatchStat.account_id.in_(team_selection[1:])
-                         )
-                      )
-                  
+        s = select(MatchStat.account_id) \
+                .where(MatchStat.game_id==game_id)
 
         with database.engine.begin() as conn:
             participants = conn.execute(s).fetchall()
-        
-        if not participants:
-            #Other members of the team aren't in the game
-            all_games.remove(game_id)
-            continue 
-        else:
-            game_count += 1
-        
-        print(participants)
-        print(len(participants))
-        
-    print(game_count)
 
-    return all_games
-    
+        participants = [p[0] for p in participants]
+        participants.sort()
+        #print("Participants {}".format(participants))
 
-"""
-    games = [x[0] for x in games]
-    
-    for game_id in games:
-        participants = MatchStat.query.filter_by(game_id=game_id).all()
-        for participant in participants:
-            if participant.account_id not in team_selection:
-                games.remove(game_id)
-                break
-   
-    return games
-"""
+        if participants == team_selection:
+            filtered_games.append(game_id)
+            continue
+
+    return filtered_games
