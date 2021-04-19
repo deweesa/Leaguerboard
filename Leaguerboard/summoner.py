f58e1af342969419cf6c8464b06caf8b84a6f5dc
Asa DeWeese <asadeweese@gmail.com>
2021-04-19 10:04:09 -0700

    Summoner stats is working again. But lose some information since splitting the tables.

--- Leaguerboard/summoner.py
+++ Leaguerboard/summoner.py
@@ -1,6 +1,6 @@
 from flask import (Blueprint, render_template, request)
 from sqlalchemy import select, text
-from Leaguerboard.models import Summoner
+from Leaguerboard.models import (Summoner, MatchStat)
 import json
 from . import database
 
@@ -21,12 +21,27 @@ def summoner_stats(summoner):
     #    win_count = conn.execute(text('select count(1) from match where summonername = :x and win = true'), x = summoner).fetchone()[0]
     #    matches = conn.execute(text('select * from match where summonername = :x'), x=summoner).fetchall()
 
+    summoner_info = Summoner.query.filter_by(name=summoner).first()
+    match_history = MatchStat.query.\
+            filter_by(account_id=summoner_info.account_id).all()
+
+    win_count = 0
+    game_count = 0
+
+    for match in match_history:
+        game_count += 1
+        if match.win: win_count += 1
+    
     with open('Leaguerboard/static/json/champion_full.json') as f:
         champ_full = json.load(f)
     
     champ_names = champ_full['keys']
 
-    matches = sorted(matches, key = lambda x:x[8], reverse=True)
+    match_history = sorted(match_history, key = lambda x:x.game_id, 
+            reverse=True)
     
+    return render_template('summoner/summoner_stat.html', summoner=summoner, 
+                            win_count=win_count, game_count=game_count, 
+                            matches=match_history, 
+                            champ_names=champ_names)
 
-    return render_template('summoner/summoner_stat.html', summoner=summoner, win_count=win_count, game_count=game_count, matches=matches, champ_names=champ_names)
