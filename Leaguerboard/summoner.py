09d2a4b6b0f8e2ca76b0aeec459457059abf96f6
Asa DeWeese <asadeweese@gmail.com>
2021-04-19 11:01:00 -0700

    Summoner stat's now hass all the stats it needs

--- Leaguerboard/summoner.py
+++ Leaguerboard/summoner.py
@@ -1,6 +1,7 @@
 from flask import (Blueprint, render_template, request)
 from sqlalchemy import select, text
-from Leaguerboard.models import (Summoner, MatchStat)
+from sqlalchemy.orm import sessionmaker
+from Leaguerboard.models import (Summoner, MatchStat, Match)
 import json
 from . import database
 
@@ -15,29 +16,29 @@ def summoner():
 
 @bp.route('/summoner/<string:summoner>')
 def summoner_stats(summoner):
-
-    #with database.engine.begin() as conn:
-    #    game_count = conn.execute(text('select count(1) from match where summonername = :x'), x=summoner).fetchone()[0]
-    #    win_count = conn.execute(text('select count(1) from match where summonername = :x and win = true'), x = summoner).fetchone()[0]
-    #    matches = conn.execute(text('select * from match where summonername = :x'), x=summoner).fetchall()
-
     summoner_info = Summoner.query.filter_by(name=summoner).first()
-    match_history = MatchStat.query.\
-            filter_by(account_id=summoner_info.account_id).all()
+
+    Session = sessionmaker(bind=database.engine)
+    session = Session()
+
+    match_history = session.query(MatchStat, Match).\
+            join(Match, MatchStat.game_id==Match.game_id, full=True).\
+            filter(MatchStat.account_id==summoner_info.account_id).\
+            all()
 
     win_count = 0
     game_count = 0
 
     for match in match_history:
         game_count += 1
-        if match.win: win_count += 1
+        if match.MatchStat.win: win_count += 1
     
     with open('Leaguerboard/static/json/champion_full.json') as f:
         champ_full = json.load(f)
     
     champ_names = champ_full['keys']
 
-    match_history = sorted(match_history, key = lambda x:x.game_id, 
+    match_history = sorted(match_history, key = lambda x:x.Match.game_id, 
             reverse=True)
     
     return render_template('summoner/summoner_stat.html', summoner=summoner, 
