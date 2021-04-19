704d45ef5c0b3cae9ad0a42f60557ad6cf114694
Asa DeWeese <asadeweese@gmail.com>
2021-04-19 08:42:49 -0700

    Summoner selection is now working, still working on summoner stat page

--- Leaguerboard/summoner.py
+++ Leaguerboard/summoner.py
@@ -1,7 +1,6 @@
 from flask import (Blueprint, render_template, request)
-#from Leaguerboard.db import get_db
-#from Leaguerboard.models import (Summoner, Match)
 from sqlalchemy import select, text
+from Leaguerboard.models import Summoner
 import json
 from . import database
 
@@ -9,18 +8,18 @@ bp = Blueprint('summoner', __name__)
 
 @bp.route('/summoners', methods=('GET', 'POST'))
 def summoner():
-    with database.engine.begin() as conn:
-        summoners = conn.execute('select summonername from summoner').fetchall()
+    summoners = Summoner.query.filter_by(is_primary=True).all()
+    summoners = [x.name for x in summoners]
 
     return render_template('summoner/summoner.html', summoners=summoners)
 
 @bp.route('/summoner/<string:summoner>')
 def summoner_stats(summoner):
 
-    with database.engine.begin() as conn:
-        game_count = conn.execute(text('select count(1) from match where summonername = :x'), x=summoner).fetchone()[0]
-        win_count = conn.execute(text('select count(1) from match where summonername = :x and win = true'), x = summoner).fetchone()[0]
-        matches = conn.execute(text('select * from match where summonername = :x'), x=summoner).fetchall()
+    #with database.engine.begin() as conn:
+    #    game_count = conn.execute(text('select count(1) from match where summonername = :x'), x=summoner).fetchone()[0]
+    #    win_count = conn.execute(text('select count(1) from match where summonername = :x and win = true'), x = summoner).fetchone()[0]
+    #    matches = conn.execute(text('select * from match where summonername = :x'), x=summoner).fetchall()
 
     with open('Leaguerboard/static/json/champion_full.json') as f:
         champ_full = json.load(f)
