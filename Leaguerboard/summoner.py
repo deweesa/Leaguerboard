7c53e9985b606588d8905a1a0de86d6fad84b4bb
Asa DeWeese <asadeweese@gmail.com>
2021-05-12 09:38:11 -0700

    Individual summoner page now has more stats for best and favorite champ
    Summoner page now has the following stats for a player's favorite and
    best champ:
        * Games Played
        * Percentage of Games Played
        * Win Percentage
        * Most Common Lane
        * Most Common Role
    
    The champions loading art is also displayed

--- Leaguerboard/summoner.py
+++ Leaguerboard/summoner.py
@@ -32,24 +32,77 @@ def summoner_stats(summoner):
     champ_games = database.session.query(MatchStat.champ, 
             func.count(MatchStat.game_id).label('count')).\
             filter(MatchStat.account_id == summoner_info.account_id).\
-            group_by(MatchStat.champ).order_by(text('count DESC')).all()
+            group_by(MatchStat.champ).order_by(text('champ DESC')).all()
 
     champ_wins = database.session.query(MatchStat.champ,
             func.count(MatchStat.game_id).label('count')).\
             filter(MatchStat.account_id == summoner_info.account_id,
                     MatchStat.win == True).group_by(MatchStat.champ).\
-            order_by(text('count DESC')).all()
+            order_by(text('champ DESC')).all()
 
     favorite_champ = {
-                        'key':champ_games[0].champ, 
-                        'games':champ_games[0].count,
-                        'wins':champ_wins[0].count,
+                        'key': -1,
+                        'games': -1,
+                        'wins': -1,
                       }
 
-    
+    best_champ =      {
+                        'key': -1,
+                        'games': -1,
+                        'wins': 0,
+                      }
+
+    champ_stats = []
+    games_index = 0
+    wins_index = 0
+
+    while games_index < len(champ_games):
+        champ = {'key': champ_games[games_index].champ}
+
+        if champ_wins[wins_index].champ != champ['key']:
+            champ['wins'] = 0
+        else:
+            champ['wins'] = champ_wins[wins_index].count
+            wins_index += 1
+
+        champ['games'] = champ_games[games_index].count
+
+        games_index += 1
+        
+        if champ['games'] > favorite_champ['games']:
+            favorite_champ = champ
+
+        if champ['wins']/champ['games'] > best_champ['wins']/best_champ['games'] \
+                and champ['games'] >= 15:
+            best_champ = champ
 
     win_count = 0
     game_count = 0
+
+    favorite_lane = database.session.query(MatchStat.lane, func.count(MatchStat.lane).label('count')).\
+                        filter(MatchStat.champ == favorite_champ['key'], 
+                               MatchStat.account_id == summoner_info.account_id).\
+                        group_by(MatchStat.lane).order_by(text('count DESC')).all()
+
+    best_lane = database.session.query(MatchStat.lane, func.count(MatchStat.lane).label('count')).\
+                        filter(MatchStat.champ == best_champ['key'],
+                               MatchStat.account_id == summoner_info.account_id).\
+                        group_by(MatchStat.lane).order_by(text('count DESC')).all()
+
+    favorite_role = database.session.query(MatchStat.role, func.count(MatchStat.role).label('count')).\
+                        filter(MatchStat.champ == favorite_champ['key'],
+                               MatchStat.account_id == summoner_info.account_id).\
+                        group_by(MatchStat.role).order_by(text('count DESC')).all()
+
+    best_role = database.session.query(MatchStat.role, func.count(MatchStat.role).label('count')).\
+                        filter(MatchStat.champ == best_champ['key'],
+                               MatchStat.account_id == summoner_info.account_id).\
+                        group_by(MatchStat.role).order_by(text('count DESC')).all()
+    
+    favorite_lane = favorite_lane[0]
+    best_lane = best_lane[0]
+    favorite_role = favorite_role[0]
+    best_role = best_role[0]
 
     for match in match_history:
         game_count += 1
@@ -67,5 +120,10 @@ def summoner_stats(summoner):
                             win_count=win_count, game_count=game_count, 
                             matches=match_history, 
                             favorite_champ=favorite_champ,
+                            best_champ=best_champ,
+                            favorite_lane=favorite_lane,
+                            favorite_role=favorite_role,
+                            best_lane=best_lane,
+                            best_role=best_role,
                             champ_names=champ_names, champ_full=champ_full['data'])
 
